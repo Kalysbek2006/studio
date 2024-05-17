b812e77bdf52c030fbf5d604c333a9a6eee520eb
baiamandev08 <mrlanovbajaman@gmail.com>
2024-05-17 18:45:34 +0300

    fghjk

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
   return (
     <>
       <div>
-          <h1 className="text-red-500 text-center">hbjnkl</h1>
+          <h1 className="text-red-500 text-center"></h1>
       </div>
     </>
   )
